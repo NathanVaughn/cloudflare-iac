8227b9ac5a9d182cddfcb14c433dbdb88b9210cc
Nathan Vaughn <nvaughn51@gmail.com>
2024-05-04 14:54:19 +0000

    Add cloudflare pages

---
+++ wrapper.py
@@ -0,0 +1,37 @@
+# Pulumi config file is completely broken, so this is a workaround
+# to run the given command with the contents of the `.env` file passed in
+
+import os
+import subprocess
+import sys
+
+THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+def read_env() -> dict:
+    env = {}
+    with open(os.path.join(THIS_DIR, ".env")) as f:
+        for line in f:
+            key, value = line.strip().split("=", 1)
+            env[key] = value
+    return env
+
+
+def main():  # sourcery skip: dict-assign-update-to-union
+    # copy existing environment
+    new_env = os.environ.copy()
+
+    # merge with .env file
+    env = read_env()
+    new_env.update(env)
+
+    # run given command
+    cmd = sys.argv[1:]
+    print("> " + " ".join(cmd))
+
+    # add poetry run to get python environment
+    subprocess.run(["poetry", "run"] + cmd, env=new_env)
+
+
+if __name__ == "__main__":
+    main()
