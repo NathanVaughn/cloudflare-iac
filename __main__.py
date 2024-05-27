092c5a381760f03060b8dd3066420aa8464e30ad
Nathan Vaughn <nvaughn51@gmail.com>
2024-05-27 20:32:43 +0000

    Import zones better

--- __main__.py
+++ __main__.py
@@ -1,4 +1,9 @@
-from iac.zones.nvaughn_email import *  # noqa
-from iac.zones.lksg_me import *  # noqa
-from iac.zones.nathanv_me import *  # noqa
-from iac.zones.nathanv_app import *  # noqa
+from iac.zones.lksg_me import ZONE as lksg_me_zone
+from iac.zones.nathanv_app import ZONE as nathanv_app_zone
+from iac.zones.nathanv_me import ZONE as nathanv_me_zone
+from iac.zones.nvaughn_email import ZONE as nvaughn_email_zone
+
+print(f"Imported configuration for {lksg_me_zone}")
+print(f"Imported configuration for {nathanv_app_zone}")
+print(f"Imported configuration for {nathanv_me_zone}")
+print(f"Imported configuration for {nvaughn_email_zone}")
