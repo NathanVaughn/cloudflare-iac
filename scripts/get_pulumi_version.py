3b4889b998b608ecc6ffb34df0114a8db2d14f67
Nathan Vaughn <nvaughn51@gmail.com>
2024-07-22 22:44:23 +0000

    Undo devcontainer in CI

---
+++ scripts/get_pulumi_version.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+import tomllib
+
+ROOT_DIR = Path(__file__).parent.parent
+
+with open(ROOT_DIR.joinpath("poetry.lock"), "rb") as fp:
+    data = tomllib.load(fp)
+print(next(p["version"] for p in data["package"] if p["name"] == "pulumi"))
